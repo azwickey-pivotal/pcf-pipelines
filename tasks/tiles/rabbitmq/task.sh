7fa20eb2de47ea0a507f24aed7afa8b297565983
azwickey-pivotal <vania12R>
2018-05-04 08:21:50 -0400

    added RMQ config props

--- tasks/tiles/rabbitmq/task.sh
+++ tasks/tiles/rabbitmq/task.sh
@@ -27,6 +27,8 @@ product_properties=$(
       ".properties.syslog_selector": { "value": "disabled" },
       ".properties.on_demand_broker_plan_1_rabbitmq_az_placement": { "value": ($azs | split(",") | map("\(.)")) },
       ".properties.on_demand_broker_plan_1_disk_limit_acknowledgement": { "value": [ "acknowledge" ] },
+      ".properties.on_demand_broker_plan_6_rabbitmq_az_placement": { "value": ($azs | split(",") | map("\(.)")) },
+      ".properties.on_demand_broker_plan_6_disk_limit_acknowledgement": { "value": [ "acknowledge" ] },
       ".rabbitmq-server.server_admin_credentials": {
         "value": {
           "identity": $rmq_user,
