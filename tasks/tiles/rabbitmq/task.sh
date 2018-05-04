11bc244e59cced80ecb5a57d93fcea8791b7a6e0
azwickey-pivotal <vania12R>
2018-05-03 23:33:46 -0400

    added rmq config

--- tasks/tiles/rabbitmq/task.sh
+++ tasks/tiles/rabbitmq/task.sh
@@ -22,8 +22,8 @@ product_properties=$(
     --arg rmq_password $RMQ_PASSWORD \
     '
     {
-      ".properties.rabbitmq-server.server_admin_credentials.identity": { "value": $rmq_user },
-      ".properties.rabbitmq-server.server_admin_credentials.password": { "value": $rmq_password },
+      ".rabbitmq-server.server_admin_credentials.identity": { "value": $rmq_user },
+      ".rabbitmq-server.server_admin_credentials.password": { "value": $rmq_password },
     }
     '
 )
