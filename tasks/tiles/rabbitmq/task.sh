3cd724bfe1455016a2a2ddf280f75d87e328e274
azwickey-pivotal <vania12R>
2018-06-04 14:29:47 -0400

    added enabling of RMQ plans

--- tasks/tiles/rabbitmq/task.sh
+++ tasks/tiles/rabbitmq/task.sh
@@ -30,7 +30,7 @@ product_properties=$(
       ".properties.on_demand_broker_plan_1_cf_service_access": { "value": [ "enable" ] },
       ".properties.on_demand_broker_plan_6_rabbitmq_az_placement": { "value": ($azs | split(",") | map("\(.)")) },
       ".properties.on_demand_broker_plan_6_disk_limit_acknowledgement": { "value": [ "acknowledge" ] },
-      ".properties.on_demand_broker_plan_6_selector.enabled.cf_service_access": { "value": [ "enable" ] },
+      ".properties.on_demand_broker_plan_6_cf_service_access": { "value": [ "enable" ] },
       ".rabbitmq-server.server_admin_credentials": {
         "value": {
           "identity": $rmq_user,
